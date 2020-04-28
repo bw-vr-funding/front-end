e29d7c12633105bf345c412bf705b2cb0c72ce63
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-04-28 15:55:19 -0400

    home page basics

--- vr-app/src/components/Home.js
+++ vr-app/src/components/Home.js
@@ -10,6 +10,21 @@ function Home(){
             <p>Create your own unique ideas, or fund fellow entrepeneurs!</p>
             <Link><button className="About">About Us</button></Link>
             {/* <Route path="/about" component={About} /> */}
+            <div id="homecar">
+                <div className="card"><h3>Create</h3>
+                <p>Post your ideas, Change the world with fellow entrepeneurs</p>
+                </div>
+                <div className="card" ><h3>Explore</h3>
+                <p>Explore what the community has to offer</p>
+                </div>
+                <div className="card" ><h3>Fund</h3>
+                <p>Fund fellow projects, help pave the way of the future</p>
+                </div>
+                <div className="card" ><h3>Imagine</h3>
+                <p>Imagine a world where ideas gained traction like no-other</p>
+                </div>
+
+            </div>
             <footer id="footer">Copyright 2020 Privacy Legal Feedback</footer>
 
         </div>
