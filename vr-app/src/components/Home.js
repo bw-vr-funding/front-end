0187ed635f95b550da818e775fb884303c9a83f4
bobbygondola <60513315+bobbygondola@users.noreply.github.com>
2020-04-29 11:29:19 -0400

    4/29/20 morning commit

--- vr-app/src/components/Home.js
+++ vr-app/src/components/Home.js
@@ -5,11 +5,14 @@ import "../index.css";
 function Home(){
     return (
         <div>
-            <h1>Welcome to VR-Funding</h1>
-            <h2>Will you help change the world?</h2>
-            <p>Create your own unique ideas, or fund fellow entrepeneurs!</p>
-            <Link><button className="About">About Us</button></Link>
-            {/* <Route path="/about" component={About} /> */}
+            <div>
+                <div></div>
+                <h1>Welcome to VR-Funding</h1>
+                <h2>Will you help change the world?</h2>
+                <p>Create your own unique ideas, or fund fellow entrepeneurs!</p>
+                <Link><button className="About">About Us</button></Link>
+                {/* <Route path="/about" component={About} /> */}
+                </div>
             <div id="homecar">
                 <div className="card"><h3>Create</h3>
                 <p>Post your ideas, Change the world with fellow entrepeneurs</p>
@@ -21,7 +24,7 @@ function Home(){
                 <p>Fund fellow projects, help pave the way of the future</p>
                 </div>
                 <div className="card" ><h3>Imagine</h3>
-                <p>Imagine a world where ideas gained traction like no-other</p>
+                <p>Imagine a world, where ideas gained traction like no-other</p>
                 </div>
 
             </div>
